b8941023c72c2ec195684c07d3cdb934155f05ae
P-denshin <zaw37875@gmail.com>
2021-02-22 21:56:54 +0900

    Roleを追加

---
+++ Resistance/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resistance {
+    /// <summary>
+    /// プレイヤーの役割
+    /// </summary>
+    enum Role {
+        /// <summary>
+        /// レジスタンス（青陣営）
+        /// </summary>
+        Resistance,
+
+        /// <summary>
+        /// スパイ（赤陣営）
+        /// </summary>
+        Spy,
+    }
+}
