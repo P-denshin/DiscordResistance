9d54d08775c3c0b0889a550917c6b26dfd9b1527
P-denshin <zaw37875@gmail.com>
2021-02-22 21:53:28 +0900

    RoundStateを追加

---
+++ Resistance/RoundState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resistance {
+    /// <summary>
+    /// 各ラウンドの状態
+    /// </summary>
+    enum RoundState {
+        /// <summary>
+        /// レジスタンス（青陣営）の勝利
+        /// </summary>
+        ResistanceWin,
+        
+        /// <summary>
+        /// スパイ（赤陣営）の勝利
+        /// </summary>
+        SpyWin,
+
+        /// <summary>
+        /// まだ
+        /// </summary>
+        NotYet,
+    }
+}
