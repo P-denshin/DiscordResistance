d62130113c5ea03259d3a9ce892443e05b53867d
P-denshin <zaw37875@gmail.com>
2021-02-23 10:10:38 +0900

    役割の決定を実装

--- Resistance/GameManager.cs
+++ Resistance/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
@@ -18,6 +19,16 @@ namespace Resistance {
         RestUserMessage board;
 
         /// <summary>
+        /// 公開チャンネル
+        /// </summary>
+        ISocketMessageChannel channel;
+
+        /// <summary>
+        /// 現在のラウンド
+        /// </summary>
+        int round;
+
+        /// <summary>
         /// 現在ゲーム中かどうかを取得する。
         /// </summary>
         public bool IsGaming { get; private set; }
@@ -27,7 +38,8 @@ namespace Resistance {
         /// </summary>
         /// <param name="discordUsers">参加ユーザのリスト</param>
         public void GameStart(List<DiscordUser> discordUsers, ISocketMessageChannel channel) {
-            initGame(discordUsers, channel);
+            this.channel = channel;
+            initGame(discordUsers);
         }
 
         /// <summary>
@@ -41,17 +53,58 @@ namespace Resistance {
         /// <summary>
         /// ゲーム開始時の初期化を行う。
         /// </summary>
-        private async void initGame(List<DiscordUser> discordUsers, ISocketMessageChannel channel) {
+        private async void initGame(List<DiscordUser> discordUsers) {
             IsGaming = true;
+            round = 0;
+
+            assignRoles(discordUsers);
+            noticeRole();
+
+            this.board = await channel.SendMessageAsync(embed: buildBoard());
+        }
+
+        /// <summary>
+        /// プレイヤーの役割決める
+        /// </summary>
+        private void assignRoles(List<DiscordUser> discordUsers) {
+            int[] number = { 2, 2, 3, 3, 3, 4 };
 
             players = new List<Player>();
-            foreach (var i in discordUsers) {
-                players.Add(new Player(i.SocketUser, Role.Spy));
+            players = players.OrderBy(a => Guid.NewGuid()).ToList();
+
+            for (var i = 0; i < discordUsers.Count; i++) {
+                DiscordUser discordUser = discordUsers[i];
+                if (i < number[discordUsers.Count - 5]) {
+                    players.Add(new Player(discordUser.SocketUser, Role.Spy));
+                } else {
+                    players.Add(new Player(discordUser.SocketUser, Role.Resistance));
+                }
             }
 
-            var boardEmbed = buildBoard();
+            players = players.OrderBy(a => Guid.NewGuid()).ToList();
+        }
 
-            this.board = await channel.SendMessageAsync(embed: boardEmbed);
+        /// <summary>
+        /// 役割を通達する。
+        /// </summary>
+        private async void noticeRole() {
+            String spyList = "";
+            foreach(var player in players) {
+                if(player.Role == Role.Spy) {
+                    spyList += "・" + player.Name + "\n";
+                }
+            }
+
+            foreach(var player in players) {
+                var dm = await player.SocketUser.GetOrCreateDMChannelAsync();
+                if (player.Role == Role.Spy) {
+                    EmbedBuilder eb = new EmbedBuilder().WithTitle("役割").WithColor(Color.Red).WithDescription("あなたはスパイです！\n\n他のスパイ\n" + spyList);
+                    await dm.SendMessageAsync(embed: eb.Build());
+                } else if(player.Role == Role.Resistance) {
+                    EmbedBuilder eb = new EmbedBuilder().WithTitle("役割").WithColor(Color.Blue).WithDescription("あなたはレジスタンスです！");
+                    await dm.SendMessageAsync(embed: eb.Build());
+                }
+            }
 
         }
 
@@ -66,10 +119,10 @@ namespace Resistance {
             // ユーザリスト
             EmbedFieldBuilder[] efb = new EmbedFieldBuilder[players.Count];
             int count = 0;
-            foreach (var user in players) {
+            foreach (var player in players) {
                 efb[count] = new EmbedFieldBuilder();
                 efb[count] = efb[count].WithIsInline(true);
-                efb[count].Name = user.Name;
+                efb[count].Name = player.Name;
                 efb[count].Value = "リーダー";
             }
             eb = eb.WithFields(efb);
