e097785808adedc40967a3570d5359f2b3a21cd0
P-denshin <zaw37875@gmail.com>
2021-02-27 16:59:24 +0900

    メッセージを同期的に受信するクラスを追加

---
+++ Resistance/MessageReceiver.cs
@@ -0,0 +1,27 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resistance {
+    static class MessageReceiver {
+        static String receivedMessage = null;
+
+        public static String ReceiveMessage(Player player) {
+            receivedMessage = null;
+            player.OnGetMessage += receive;
+
+            while (receivedMessage == null)
+                ;
+
+            var res = new String(receivedMessage);
+
+            return res;
+        }
+
+        private static void receive(SocketMessage message) {
+            receivedMessage = message.Content;
+        }
+
+    }
+}
